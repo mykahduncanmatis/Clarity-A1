04a1b2aef0601250ce1933a9d734727fc763c90f
mykahduncanmatis <mykah.duncanmatis@uleth.ca>
2022-11-01 21:38:52 -0600

    FinalCommit(Hopefully)

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,55 +1,16 @@
-var app = new Framework7({
-    // App root element
-    el: '#app',
-    // ... other parameters
-  });
-  var mainView = app.views.create('.view-main')
-  
-//Plan
-//terrarium on the home screen
-//light on/off
-//temperature
-//food
-//humidity
-//icons for the functions
+//sidebar 
+//tutorial on https://www.youtube.com/watch?v=dEdwDijoceY
+//I tried to get the framework7 side panel to work but it totally made everything else stop functioning and still wouldnt work
+const navBar = document.querySelector("nav"),
+menuBtns = document.querySelectorAll(".menu-icon"),
+overlay = document.querySelectorAll(".overlay");
 
-//if #bug is clicked then food level increases
-//humidity decreases over time
-//tempurature decreases over time
-
-//humidity
-var humidity = 50;
-var dry = 0;
-var soggy = 100;
-
-
-
-var neardeath = false; //frog is healthy
-
-
-
-
-function dryout() {
-    humidity--; //every time this func runs, water level decreases by 1
-    console.log(humidity)
-    checkhealth();
-    var humiditytimer = setTimeout(dryout,500); // for timing (example on w3 schools setTimeout()) in milliseconds
-  }
-  
-  dryout() //call function after it is declared
-  
-
-function checkhealth() {
-    //if(humidity <= nowater || humidity >= drowing) {
-        neardeath = true;
-        console.log("help");
-    //}
-    //else(neardeath && humidity > dry) {
-    //    neardeath = false;
-
-   // }
-}
-
-$("#drops").on("click", function() {
-    humidity += 20;
+menuBtns.forEach(menuBtn => {
+    menuBtn.addEventListener("click", () => {
+        navBar.classList.toggle("open");
+    });
+});
+    
+overlay.addEventListener("click", () => {
+    navBar.classList.remove("open");
 })
